dd478c9664eccb9abc7cf72b316640f5514c3433
Andy <aking27@wisc.edu>
2020-02-03 14:42:57 -0600

    Test 2

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //ImageView imgview
+    //ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
